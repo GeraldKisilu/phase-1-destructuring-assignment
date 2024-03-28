33089dd3cc6235f350a2e08d24bdd6355dfe73e6
geraldkisilu <muligerald6@gmail.com>
2024-03-28 10:13:43 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,14 +1,58 @@
-const farmAnimals = 'cow horse sheep pig chicken';
+const farmAnimals = 'cow,horse,sheep,pig,chicken';
+const AnimalSounds ={
+  neigh: 'horse',
+  baa:'sheep',
+  oink: 'pig',
+  cluck: 'chicken',
+  moo:'cow',
+}
+const {neigh,baa,oink,cluck,moo} = AnimalSounds;
+console.log(AnimalSounds)
+
+const traditionalNames = {
+dolly: 'sheep',
+bessie : 'cow',
+babe: 'pig',
+little: 'chicken'
+}
+const {dolly,bessie,babe,little} = traditionalNames;
+console.log(traditionalNames)
+
+const AnimalColors ={
+  pink : 'pig',
+  blackAndWhite :'cow',
+  black: 'sheep',
+  }
+  const {pink,blackAndWhite,black} = AnimalColors;
+  console.log(AnimalColors)
+
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+console.log(colors)
+const sixColors = {
+  r : red,
+  o : orange,
+  y : yellow,
+  g : green,
+  b : blue,
+  v: violet,
+  indg: indigo,
+}
+const{r, o, y,g,b,v,indg} = sixColors
+console.log(sixColors)
+
 
 const muppet = {
   muppetName: 'Miss Piggy',
   color: 'pink',
   song: 'Never Before, Never Again',
   job: 'Cast member of The Muppet Show',
-  partner: 'Kermit'
+  partner: 'Kermit',
 };
+const{muppetName,color,song,job,partner} =muppet
+console.log(muppet);
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +68,15 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+const finish = {
+  song2:'Moving Right Along',
+  song4:'I Hope That Something Better Comes Along',
+  nestedJob: 'Host of The Muppet Show',
+  nestedPartner: 'Miss Piggy',
+};
+
+const {  song2 , song4, nestedJob, nestedPartner } = finish
+console.log(finish)
 
 // Strings
 
@@ -39,7 +92,7 @@ const nestedMuppet = {
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+// 6. But wait! Indigo is now feeling super left out. Let's only assign indigo using indg. 
 
 // Objects
 
